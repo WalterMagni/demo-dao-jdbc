3e72febcaca80baf1077b862c4cdefc1906d6c52
Walter Magni <waltermagni@terra.com.br>
2020-01-28 10:55:46 -0300

    add Department

---
+++ src/br/com/demodao/application/Program.java
@@ -0,0 +1,14 @@
+package br.com.demodao.application;
+
+import br.com.demodao.model.entities.Department;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Department dp1 = new Department(1, "Books");
+		System.out.println(dp1);
+
+	}
+
+}
---
+++ src/br/com/demodao/db/DBIntegratyException.java
@@ -0,0 +1,10 @@
+package br.com.demodao.db;
+
+public class DBIntegratyException extends RuntimeException{
+	private static final long serialVersionUID = 1L;
+
+	public DBIntegratyException(String msg) {
+		super(msg);
+	}
+	
+}
