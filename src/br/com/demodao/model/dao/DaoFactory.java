acad28d65ec236e7563e5443725f6438bbebc91f
Walter Magni <waltermagni@terra.com.br>
2020-01-28 11:11:02 -0300

    SellerDaoJDBC and DaoFactory

---
+++ src/br/com/demodao/model/dao/DaoFactory.java
@@ -0,0 +1,11 @@
+package br.com.demodao.model.dao;
+
+import br.com.demodao.model.dao.impl.SellerDaoJDBC;
+
+public class DaoFactory {
+
+	public static SellerDao createSellerDao() {
+		return new SellerDaoJDBC();
+	}
+	
+}
