6624ad3130771af847d677f6efebba0b0c98f89a
Walter Magni <waltermagni@terra.com.br>
2020-01-28 11:05:25 -0300

    DepartmentDao and SellerDao interfaces

---
+++ src/br/com/demodao/model/dao/DepartmentDao.java
@@ -0,0 +1,15 @@
+package br.com.demodao.model.dao;
+
+import java.util.List;
+
+import br.com.demodao.model.entities.Department;
+
+public interface DepartmentDao {
+
+	void insert(Department obj);
+	void update(Department obj);
+	void deleteById(Integer id);
+	Department findById(Integer id);
+	List<Department> findAll();
+	
+}
